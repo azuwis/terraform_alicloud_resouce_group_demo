bb6b1aaa3498b024175ffcbd99edea3bfd7451e9
Zhong Jianxin <azuwis@gmail.com>
2020-08-19 14:42:19 +0800

    升级 alicloud provider 到 1.93

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "~> 1.93"
+    }
+  }
+}
+
 variable "access_key" {}
 variable "secret_key" {}
 
@@ -5,5 +15,4 @@ provider "alicloud" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = "cn-hangzhou"
-  version    = "~> 1.92"
 }
